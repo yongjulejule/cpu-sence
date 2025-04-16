815a97daa24bb7de9b77e9e7373f6f2599a07ea6
yongjulejule <lyjshow200@gmail.com>
2025-04-16 12:46:30 +0000

    feat: Add memory alloc test

--- phase-1-basic-ops/3-memory-operations/memory_alloc.c
+++ phase-1-basic-ops/3-memory-operations/memory_alloc.c
@@ -2,15 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#define ALLOC_SIZE 10000000
+#define CHUNK_COUNT 24400
+#define CHUNK_SIZE 4096 
+
 
 int main() {
     struct timespec start, end;
     clock_gettime(CLOCK_MONOTONIC, &start);
-    char *buffer = (char*)malloc(ALLOC_SIZE);
-    memset(buffer, 'A', ALLOC_SIZE);
-    char *copy = (char*)malloc(ALLOC_SIZE);
-    memcpy(copy, buffer, ALLOC_SIZE);
+
+	unsigned char **base = calloc(CHUNK_COUNT + 1, sizeof(unsigned char*));
+
+	for (int i = 0; i < CHUNK_COUNT; i++) {
+		base[i] = (char*)calloc(CHUNK_SIZE + 1, sizeof(char));
+		memset(base[i], 'A', CHUNK_SIZE);
+	}
+
     clock_gettime(CLOCK_MONOTONIC, &end);
     long seconds = end.tv_sec - start.tv_sec;
     long ns = end.tv_nsec - start.tv_nsec;
@@ -18,8 +24,16 @@ int main() {
         seconds--;
         ns += 1000000000;
     }
-    printf("Memory operations completed in: %ld.%09ld seconds\n", seconds, ns);
-    free(buffer);
-    free(copy);
+	int total_size = 0;
+	for (int i = 0; base[i]; i++) {
+		for (int j = 0; base[i][j]; j++) {
+			++total_size;
+		}
+	}
+    printf("Memory operations completed in: %ld.%09ld seconds with %d blocks\n", seconds, ns, total_size);
+	for (int i = 0; base[i]; i++) {
+		free(base[i]);
+	}
+    free(base);
     return 0;
 }
--- setup.sh
+++ setup.sh
@@ -12,3 +12,10 @@ sudo apt install -y linux-tools-common linux-tools-$(uname -r) time strace
 echo "perf 권한 완화"
 echo 'kernel.perf_event_paranoid = -1' | sudo tee /etc/sysctl.d/99-perf.conf
 sudo sysctl --system
+
+function 편의기능 {
+		echo "OMB 설치"
+		bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+}
+
+편의기능
