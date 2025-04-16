a1667ccfff5d248cf4dbb51f2a07ed689ac83e0e
yongjulejule <lyjshow200@gmail.com>
2025-04-16 11:55:01 +0000

    feat: Update branch prediction case

--- phase-1-basic-ops/2-loop-branch-recursion/branch_predictable.c
+++ phase-1-basic-ops/2-loop-branch-recursion/branch_predictable.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
 #define N 1000000000UL
@@ -6,14 +7,20 @@
 int main() {
     volatile unsigned long long sum = 0;
     struct timespec start, end;
+
     clock_gettime(CLOCK_MONOTONIC, &start);
+    srand(0);
     for (unsigned long i = 0; i < N; i++) {
-        if (i < N) sum++;
+		int r =  rand() & 0xff;
+		if (i & 1 == 0) {
+			sum += r;
+		}
     }
     clock_gettime(CLOCK_MONOTONIC, &end);
     long seconds = end.tv_sec - start.tv_sec;
     long ns = end.tv_nsec - start.tv_nsec;
     if (ns < 0) { seconds--; ns += 1000000000; }
+
     printf("Predictable branch result: %llu\n", sum);
     printf("Elapsed time: %ld.%09ld seconds\n", seconds, ns);
     return 0;
--- phase-1-basic-ops/2-loop-branch-recursion/branch_unpredictable.c
+++ phase-1-basic-ops/2-loop-branch-recursion/branch_unpredictable.c
@@ -2,34 +2,26 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define N 100000000UL
-
-int h0() { return 0; }
-int h1() { return 1; }
-int h2() { return 2; }
-int h3() { return 3; }
-int h4() { return 4; }
-int h5() { return 5; }
-int h6() { return 6; }
+#define N 1000000000UL
 
 int main() {
-		volatile unsigned long long sum = 0;
-		struct timespec start, end;
-		clock_gettime(CLOCK_MONOTONIC, &start);
-		srand(time(NULL)); // 시드 랜덤화로 매번 패턴 다르게
-		int (*handlers[7])(void) = { h0, h1, h2, h3, h4, h5, h6 };
+    volatile unsigned long long sum = 0;
+    struct timespec start, end;
 
-		for (int i = 0; i < N; i++) {
-				int r = rand();
-				int index = r % 7;
-				sum += handlers[index]();  // 간접 분기
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    srand(0);
+    for (unsigned long i = 0; i < N; i++) {
+     	int r = rand() & 0xff;
+		if (r & 1) {
+			sum += r;
 		}
-		clock_gettime(CLOCK_MONOTONIC, &end);
-		long seconds = end.tv_sec - start.tv_sec;
-		long ns = end.tv_nsec - start.tv_nsec;
-		if (ns < 0) { seconds--; ns += 1000000000; }
-		printf("Unpredictable branch result: %llu\n", sum);
-		printf("Elapsed time: %ld.%09ld seconds\n", seconds, ns);
-		return 0;
-}
+    }
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    long seconds = end.tv_sec - start.tv_sec;
+    long ns = end.tv_nsec - start.tv_nsec;
+    if (ns < 0) { seconds--; ns += 1000000000; }
 
+    printf("Unpredictable branch result: %llu\n", sum);
+    printf("Elapsed time: %ld.%09ld seconds\n", seconds, ns);
+    return 0;
+}
